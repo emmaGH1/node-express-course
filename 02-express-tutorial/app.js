eed14fbad84fd1657d33911e387bd399d08c2ad9
Maduakor Emmanuel <emma080355@gmail.com>
2023-01-09 01:13:26 +0100

    lil update

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,27 @@
-console.log('Express Tutorial')
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('./navbar-app'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page')
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send(`<div><h1>Error!!</h1><br>Page not found</div>`)
+})
+
+app.listen(5000, () => {
+    console.log('server is listening on port 5000...')
+})
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.use
